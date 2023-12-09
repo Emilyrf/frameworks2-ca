7277f4a677665ca1dcf46c01d08615031a873049
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-12-09 13:01:29 +0100

    Creates api hook

---
+++ src/hooks/useApi.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+export default function useApi(url) {
+    const [products, setProducts] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+  
+    useEffect(() => {
+      async function getData() {
+        try {
+          setIsLoading(true);
+          setIsError(false);
+          const fetchedData = await fetch(url);
+          if (!fetchedData.ok) {
+            throw new Error(`HTTP error! Status: ${fetchedData.status}`);
+          }
+          const json = await fetchedData.json();
+          setProducts(json);
+        } catch (error) {
+          console.log(error);
+          setIsError(true);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+  
+      getData();
+    }, [url]);
+    return { products, isLoading, isError };
+  }
