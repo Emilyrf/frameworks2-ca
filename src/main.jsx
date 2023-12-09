bb0273c802bd57727eb358639eb54f1e1c55b358
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-12-09 12:03:46 +0100

    Import AppRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Routes from './Routes.jsx'
-import './index.css'
+import AppRoutes from './Routes.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Routes />
+    <AppRoutes />
   </React.StrictMode>,
 )
