22764e9a346e9ce77f6c2d2a8293ecfac64e5e73
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-12-09 12:01:27 +0100

    Create approutes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/Nav";
+import Homepage from "./pages/Home";
+import ContactPage from "./pages/Contact";
+import CartPage from "./pages/Cart";
+import ProductPage from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
-
-function Routes() {
+function AppRoutes() {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/contact" element={<ContactPage/>} />
+          <Route path="/cart" element={<CartPage />} />
+
+          <Route path="/product/:id" element={<ProductPage />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
   )
   
 }
 
-export default Routes
+export default AppRoutes
