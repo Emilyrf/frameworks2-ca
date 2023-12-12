443f11d81ef100ce3fc9cf38617967bf83911f39
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-12-12 14:01:41 +0100

    Implements search

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,25 @@
 import Banner from "../../components/Banner";
 import useApi from '../../hooks/useApi';
 import ProductCard from "../../components/ProductCard";
+import { useState } from "react";
 
 export default function Homepage() {
     const { products, isLoading, isError } = useApi(
         'https://api.noroff.dev/api/v1/online-shop',
     );
 
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredProducts = products.filter((product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  
+    const handleSearchChange = (e) => {
+      setSearchTerm(e.target.value);
+    };
+
     if (isLoading) {
-        return <div>Loading</div>;
+        return <span className="loading loading-spinner loading-lg"></span>
     }
 
     if (isError) {
@@ -18,8 +29,15 @@ export default function Homepage() {
         <main>
             <Banner />
             <h1 className="text-3xl font-bold m-5">Homepage</h1>
+            <input
+                type="text"
+                placeholder="Search products..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="mt-3 p-2 border border-gray-300 rounded-md"
+            />
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-8">
-                {products.map((product) => (
+                {filteredProducts.map((product) => (
                     <div key={product.id} className="card card-compact bg-base-100 shadow-xl h-96">
                         <ProductCard product={product} />
                     </div>
