957374eb50286972d77d01668c8d962fc7de52a1
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-12-14 12:09:59 +0100

    Styles banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,15 +1,21 @@
 export default function Banner() {
-    return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row">
-    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+  return (
+    <div className="grid grid-flow-col grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 gap-4 bg-base-200">
+      <div className="flex items-center justify-center">
+        <main className="max-w-screen-xl px-4 lg:px-16">
+          <div className="text-left">
+            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Welcome to
+              <br />
+              <span className="text-primary">Mercado</span>!
+            </h2>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Here you will find everything you need with the prices you ever wanted.
+            </p>
+          </div>
+        </main>
+      </div>
+      <img className="w-full object-cover h-72 md:h-screen bg-cover bg-center" src="/assets/hero.jpeg" alt="Woman with shopping bags and celphone." />
     </div>
-  </div>
-</div>
-    )
-
+  );
 }
