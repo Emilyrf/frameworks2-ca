fb8d47922a4cf58717da9db364e3eb4ccede6ed2
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-12-18 13:10:48 +0100

    Styles page

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -28,7 +28,9 @@ export default function ProductPage() {
         <img className="max-w-sm rounded-lg shadow-2xl" src={products.imageUrl} alt={products.title} />
         <div>
           <h1 className="text-2xl font-bold mt-4">{products.title}</h1>
-          <span className="text-gray-500">{products.rating} <FaStar></FaStar></span>
+          <span className="text-gray-500 inline-flex items-center">
+            {products.rating} <FaStar className="ml-1 text-warning" />
+          </span>
           <p className="py-6">{products.description}</p>
 
           {products.discountedPrice > 0 && products.discountedPrice < products.price ? (
@@ -36,36 +38,52 @@ export default function ProductPage() {
               <p className="text-lg font-bold">Discounted Price: ${products.discountedPrice}</p>
               <div className="flex items-center">
                 <span className="mr-2 text-gray-500">Originally: </span>
-                <span className="line-through">${products.price}</span>
+                <span className="line-through text-gray-500">${products.price}</span>
                 {discountPercentage && <span className="text-green-600 ml-2">Save {discountPercentage}</span>}
               </div>
             </>
           ) : (
-            <p>${products.price}</p>
+            <p className="text-lg font-bold">Price: ${products.price}</p>
           )}
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={() => addToCart(products)}>Add to cart</button>
+          </div>
 
-          <button className="btn btn-primary" onClick={() => addToCart(products)}>Add to cart</button>
-        </div>
 
-      </div>
-
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Product Reviews</h2>
-          {products.reviews && products.reviews.length > 0 ? (
-            <ul className="mt-4">
-              {products.reviews.map((review) => (
-                <li key={review.id} className="mb-4">
-                  <p className="text-lg font-semibold">{review.username} rated it {review.rating} stars</p>
-                  <p className="mt-2">{review.description}</p>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="mt-4">No reviews available for this product.</p>
-          )}
         </div>
       </div>
+
+      <div className="flex flex-col items-center  mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-4 text-primary">Reviews</h2>
+
+        {products.reviews && products.reviews.length > 0 ? (
+          <div className="grid gap-4">
+            {products.reviews.map((review) => (
+              <div key={review.id} className="card w-full bg-base-100 shadow-xl">
+                <div className="card-body">
+                  <p className="card-title">
+                    {review.username} rated it {review.rating} stars:
+                  </p>
+                  <p>{review.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="grid gap-4">
+
+            <div className="card w-full bg-base-100 shadow-xl">
+              <div className="card-body">
+                <p className="card-title">
+                  There are no reviews for this product yet.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        )}
+      </div>
+
     </>
 
   );
