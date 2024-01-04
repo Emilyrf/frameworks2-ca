a57e7335c24085ed1c66704f598171ff711ee289
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-01-04 18:07:56 +0100

    Adds useLocalStorage hook

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,19 +1,16 @@
-import { createContext, useState, useEffect } from 'react'
+import React, { createContext, useEffect } from 'react';
+import { useLocalStorage } from '@uidotdev/usehooks';
 
-export const CartContext = createContext()
+export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState(
-    localStorage.getItem('cartItems')
-      ? JSON.parse(localStorage.getItem('cartItems'))
-      : [],
-  )
-  const [showSuccessAlert, setShowSuccessAlert] = useState(false)
-  const [isAddToCartDisabled, setIsAddToCartDisabled] = useState(false)
+  const [cartItems, setCartItems] = useLocalStorage('cartItems', []);
+  const [showSuccessAlert, setShowSuccessAlert] = useLocalStorage('showSuccessAlert', false);
+  const [isAddToCartDisabled, setIsAddToCartDisabled] = useLocalStorage('isAddToCartDisabled', false);
 
   const addToCart = (item) => {
-    setIsAddToCartDisabled(true)
-    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id)
+    setIsAddToCartDisabled(true);
+    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
 
     if (isItemInCart) {
       setCartItems(
@@ -22,23 +19,23 @@ export const CartProvider = ({ children }) => {
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem,
         ),
-      )
+      );
     } else {
-      setCartItems([...cartItems, { ...item, quantity: 1 }])
+      setCartItems([...cartItems, { ...item, quantity: 1 }]);
     }
 
-    setShowSuccessAlert(true)
+    setShowSuccessAlert(true);
     setTimeout(() => {
-      setIsAddToCartDisabled(false)
-      setShowSuccessAlert(false)
-    }, 3000)
-  }
+      setIsAddToCartDisabled(false);
+      setShowSuccessAlert(false);
+    }, 3000);
+  };
 
   const removeFromCart = (item) => {
-    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id)
+    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
 
     if (isItemInCart.quantity === 1) {
-      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id))
+      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id));
     } else {
       setCartItems(
         cartItems.map((cartItem) =>
@@ -46,33 +43,27 @@ export const CartProvider = ({ children }) => {
             ? { ...cartItem, quantity: cartItem.quantity - 1 }
             : cartItem,
         ),
-      )
+      );
     }
-  }
+  };
 
   const clearCart = () => {
-    setCartItems([])
-  }
+    setCartItems([]);
+  };
 
   const getCartTotal = () => {
     const total = cartItems.reduce(
       (total, item) => total + item.discountedPrice * item.quantity,
       0,
-    )
+    );
 
-    return total.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
-  }
-
-  useEffect(() => {
-    localStorage.setItem('cartItems', JSON.stringify(cartItems))
-  }, [cartItems])
+    return total.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  };
 
   useEffect(() => {
-    const cartItems = localStorage.getItem('cartItems')
-    if (cartItems) {
-      setCartItems(JSON.parse(cartItems))
-    }
-  }, [])
+    // Save cartItems to localStorage when it changes
+    setCartItems(cartItems);
+  }, [cartItems, setCartItems]);
 
   return (
     <CartContext.Provider
@@ -88,5 +79,5 @@ export const CartProvider = ({ children }) => {
     >
       {children}
     </CartContext.Provider>
-  )
-}
+  );
+};
