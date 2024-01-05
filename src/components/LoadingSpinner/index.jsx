6e69069d6f9386c0c261aa63fcb34cc58d0317af
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-01-05 16:51:23 +0100

    Centralizes loader on the page

--- src/components/LoadingSpinner/index.jsx
+++ src/components/LoadingSpinner/index.jsx
@@ -1,5 +1,7 @@
 export const LoadingSpinner = () => {
   return (
-    <span className='loading loading-spinner loading-xl h-40 w-40 text-primary'></span>
+    <div className='flex items-center justify-center h-screen'>
+      <span className='loading loading-spinner loading-xl h-40 w-40 text-primary'></span>
+  </div>
   )
 }
