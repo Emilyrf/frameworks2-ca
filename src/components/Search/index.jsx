c5cebd4368c2cbea1dd4ffc3f8124dd9127d563c
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-01-05 18:28:28 +0100

    Refactors to show a dropdown search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,36 @@
-const SearchInput = ({ value, onChange }) => (
-  <input
-    type='text'
-    placeholder='Search products...'
-    value={value}
-    onChange={onChange}
-    className='input input-bordered input-primary w-full max-w-xs sm:max-w-md ml-4'
-  />
-)
+import { useState } from "react"
+import { Link } from 'react-router-dom';
 
-export default SearchInput
+function SearchInput({ products = [] }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div className="ml-2">
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="input input-bordered input-primary w-full max-w-md"
+      />
+      {searchTerm && (
+        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 absolute">
+          {filteredProducts.map((product) => (
+            <li key={product.id}>
+              <Link
+                to={`/product/${product.id}`}
+                className="block px-4 py-2 hover:bg-gray-100"
+              >
+                {product.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+
+  )
+}
+export default SearchInput;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,14 @@
 import Banner from '../../components/Banner'
 import useApi from '../../hooks/useApi'
 import { API_BASE_URL } from '../../components/constants/api'
-import useProductSearch from '../../hooks/useProductSearch'
 import SearchInput from '../../components/Search'
 import ProductsGrid from '../../components/ProductsGrid'
 import { LoadingSpinner } from '../../components/LoadingSpinner'
 import AlertError from '../../components/AlertError'
 
+
 export default function Homepage() {
   const { products, isLoading, isError } = useApi(API_BASE_URL)
-
-  const { searchTerm, filteredProducts, handleSearchChange } =
-    useProductSearch(products)
 
   if (isLoading) {
     return <LoadingSpinner />
@@ -27,15 +24,10 @@ export default function Homepage() {
       <Banner />
       <div className='flex justify-between items-center m-5'>
         <h1 className='text-3xl font-bold text-primary '>Homepage</h1>
-        <SearchInput value={searchTerm} onChange={handleSearchChange} />
+        <SearchInput products={products} />
       </div>
-      {filteredProducts.length === 0 ? (
-        <div className='flex items-center justify-center'>
-          <p className='text-gray-600'>Product not found.</p>
-        </div>
-      ) : (
-        <ProductsGrid products={filteredProducts} />
-      )}
+      <ProductsGrid products={products} />
+      )
     </main>
   )
 }
