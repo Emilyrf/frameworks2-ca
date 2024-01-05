b07eb0d225e5ebaf55f0f1b50ac685bde4a57abb
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-01-05 18:59:22 +0100

    Debug

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <div className='grid grid-flow-col grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 gap-4 bg-base-200'>
       <div className='flex items-center justify-center'>
-        <main className='max-w-screen-xl px-4 lg:px-16'>
+        <div className='max-w-screen-xl px-4 lg:px-16'>
           <div className='text-left'>
             <h2 className='text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl'>
               Welcome to
@@ -14,7 +14,7 @@ export default function Banner() {
               wanted.
             </p>
           </div>
-        </main>
+        </div>
       </div>
       <img
         className='w-full object-cover h-72 md:h-screen bg-cover bg-center'
